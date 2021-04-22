7354a3cef04c03092717c7c12748b27859e917d0
Soupiershoe7 <katarina@bitmonster.org>
2021-04-22 09:59:44 -0500

    fix print randnum

--- thermometer.py
+++ thermometer.py
@@ -9,9 +9,9 @@ def thermometer (T):
     for i in range (int(T) - 90):
         pixels[i] = (10,0,0)
 
-randnum = random.randint (90,110)
 
 for i in range (10):
+    randnum = random.randint (90,110)
     temp= (randnum)
     print (randnum)
     thermometer (temp)
