e1dedb1efad87e128c09f58430864d56a55330b4
Soupiershoe7 <katarina@bitmonster.org>
2021-04-22 09:55:50 -0500

    randint gen

--- thermometer.py
+++ thermometer.py
@@ -1,5 +1,7 @@
 import board
 import neopixel
+import time
+import random
 
 pixels = neopixel.NeoPixel(board.D18, 20)
 
@@ -7,7 +9,7 @@ def thermometer (T):
     for i in range (int(T) - 90):
         pixels[i] = (10,0,0)
 
-
-temp= 97.56
-
-thermometer (temp)
+for i in range (10):
+    temp= (random.randint (90,110))
+    thermometer (temp)
+    time.sleep (1)
