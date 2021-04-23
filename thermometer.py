c6b664f9a04fabb923ee7e01c7f9c04689975224
Soupiershoe7 <katarina@bitmonster.org>
2021-04-23 08:29:10 -0500

    off

--- thermometer.py
+++ thermometer.py
@@ -9,12 +9,12 @@ def thermometer (T):
     for i in range (int(T) - 90):
         pixels[i] = (10,0,0)
 
-#def off ():
-#    for i in range (20):
-#        pixels[i] = (0,0,0)
+def off ():
+    for i in range (20):
+        pixels[i] = (0,0,0)
 
 for i in range (10):
-#    off ()
+    off ()
     randnum = random.randint (90,110)
     temp = (randnum)
     print (randnum)
